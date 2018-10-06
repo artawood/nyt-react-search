582bbb94eb0b4c3317ea3312f1883df5f16c6be9
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-10-05 23:45:05 -0700

    initial MERN app setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Jumbotron from './components/Jumbotron';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,26 @@
+const bodyParser = require('body-parser');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
+  });
